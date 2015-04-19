834a31ed260cbc6a687233eda2fa8c31ddb3b670
kk6axq <kk6axq@users.noreply.github.com>
2015-04-19 16:54:01 -0400

    Updated to my newest version

--- ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.ino
+++ ros_arduino_firmware/src/libraries/ROSArduinoBridge/motor_driver.ino
@@ -9,66 +9,17 @@
 
 #ifdef USE_BASE
 #include "commands.h";
-#if defined POLOLU_VNH5019
-  /* Include the Pololu library */
-  #include "DualVNH5019MotorShield.h"
 
-  /* Create the motor driver object */
-  DualVNH5019MotorShield drive;
-  
-  /* Wrap the motor driver initialization */
-  void initMotorController() {
-    drive.init();
-  }
-
-  /* Wrap the drive motor set speed function */
-  void setMotorSpeed(int i, int spd) {
-    if (i == LEFT) drive.setM1Speed(spd);
-    else drive.setM2Speed(spd);
-  }
-
-  // A convenience function for setting both motor speeds
-  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-    setMotorSpeed(LEFT, leftSpeed);
-    setMotorSpeed(RIGHT, rightSpeed);
-  }
-#elif defined POLOLU_MC33926
-  /* Include the Pololu library */
-  #include "DualMC33926MotorShield.h"
-
-  /* Create the motor driver object */
-  DualMC33926MotorShield drive;
-  
-  /* Wrap the motor driver initialization */
-  void initMotorController() {
-    drive.init();
-  }
-
-  /* Wrap the drive motor set speed function */
-  void setMotorSpeed(int i, int spd) {
-    if (i == LEFT) drive.setM1Speed(spd);
-    else drive.setM2Speed(spd);
-  }
-
-  // A convenience function for setting both motor speeds
-  void setMotorSpeeds(int leftSpeed, int rightSpeed) {
-    setMotorSpeed(LEFT, leftSpeed);
-    setMotorSpeed(RIGHT, rightSpeed);
-  }
-
-#elif defined BOT
+//#include <Servo.h>
+//Servo sabertoothRightChannel;
+//Servo sabertoothLeftChannel;
 void initMotorController() {
-Servo sabertoothRightChannel;
-Servo sabertoothLeftChannel;
-sabertoothRightChannel.attach(sabertoothRightPin, 1000, 2000);
-sabertoothLeftChannel.attach(sabertoothLeftPin, 1000, 2000);
-sabertoothRightChannel.write(90);
-sabertoothLeftChannel.write(90);
-
+Serial2.begin(9600);
+Serial2.write(0);//tell the sabertooth to stop both motors. 
 }
 boolean directionLeft = false;
 boolean directionRight = false;
-boolean direction(int i){
+boolean direction(int i){//return the direction of rotation of a wheel
    if(i == LEFT){
       return directionLeft;
    }else{
@@ -76,21 +27,35 @@ boolean direction(int i){
    }
 
 }
-void setMotorSpeed(int i, int spd) {
+void setMotorSpeed(int i, int spd) {//send a command to the sabertooth to set a motor speed
 if (i == LEFT){
-if(spd < 90){
+  
+if(spd < MIDSABERTOOTH){
 directionLeft == BACKWARDS;
 }else{
 directionLeft == FORWARDS;
 }
-sabertoothLeftChannel.write(constrain(spd, MINSABERTOOTH, MAXSABERTOOTH));
+
+if(abs(spd-MIDSABERTOOTH) < 8){
+  Serial2.write(64);
 }else{
-if(spd < 90){
+Serial2.write(map(spd, -255, 255, 1, 127));
+Serial.println("The value has passed");
+Serial.println(map(spd, -255, 255, 1, 127));
+}
+
+//Serial.println(spd);
+
+//sabertoothLeftChannel.write(constrain(map(spd, -255, 255, 0, 180), MINSABERTOOTH, MAXSABERTOOTH));
+}else{
+  
+if(spd < MIDSABERTOOTH){
 directionRight == BACKWARDS;
 }else{
 directionRight == FORWARDS;
 }
-sabertoothRightChannel.write(constrain(spd, MINSABERTOOTH, MAXSABERTOOTH));
+Serial2.write(map(spd, -255, 255, 128, 255));
+//sabertoothRightChannel.write(constrain(map(spd, -255, 255, 0, 180), MINSABERTOOTH, MAXSABERTOOTH));
 }
 }
 // A convenience function for setting both motor speeds
@@ -99,8 +64,6 @@ setMotorSpeed(LEFT, leftSpeed);
 setMotorSpeed(RIGHT, rightSpeed);
 }
 
-#else
-  #error A motor driver must be selected!
-#endif
+
 
 #endif
